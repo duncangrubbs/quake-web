4ce9e25a153eaf074824b353f86d4134540b29a3
duncangrubbs <duncan.grubbs@gmail.com>
2016-05-25 12:01:35 -0700

    Begin work with vanilla temp.

--- scripts/script.js
+++ scripts/script.js
@@ -1,25 +1,22 @@
-function query(){
-	var mag = document.getElementById('magnitude').value;
-	var lat = document.getElementById('latitude').value;
-	var long = document.getElementById('longitude').value;
-	var radius = document.getElementById('radius').value;
+function query(finalURL, request){
 
-	var request = new XMLHttpRequest();
-	var url = "http://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime=2016-01-01&endtime=2016-04-20&limit=200";
-	var finalURL = url + "&minmagnitude=" + mag + "&latitude=" + lat + "&longitude=" + long + "&maxradius=" + radius;
 	request.open('GET', finalURL, false);
 	request.send(null);
 
 	var data = JSON.parse(request.response);
+	console.log(finalURL);
 	var features = data.features;
 	var properties = features.properties;
+	console.log(features[0].properties.mag);
 
 	for(var i = 0; i < features.length; i++){
-		features[i].properties.timeString = timeConverter(features[i].properties.time);
+		var temp = features[i].properties.place;
+		document.write(temp);
+		// features[i].properties.timeString = timeConverter(features[i].properties.time);
 	}
 
-	var repeatTemp = document.querySelector('#repeat-temp');
-	repeatTemp.items = features;
+	// var repeatTemp = document.querySelector('#repeat-temp');
+	// repeatTemp.items = features;
 
 	return false;
 }
@@ -35,4 +32,22 @@ function timeConverter(time){
   var sec = a.getSeconds();
   var finalDate = month + ' ' + date + ', ' + year + ' at ' + hour + ':' + min + ':' + sec;
   return finalDate;
+}
+
+function getURl() {
+	if ("geolocation" in navigator) {
+		navigator.geolocation.getCurrentPosition(getPosition);
+	} else {
+	  alert('Your browser does not support geolocation :(');
+	}
+
+}
+
+function getPosition (position) {
+	var lat = position.coords.latitude;
+	var long = position.coords.longitude;
+	var request = new XMLHttpRequest();
+	var url = "http://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime=2016-01-01&endtime=2016-04-20";
+	var finalURL = url + "&minmagnitude=" + 4 + "&latitude=" + lat + "&longitude=" + long + "&maxradius=" + 5;
+	query(finalURL, request);
 }
